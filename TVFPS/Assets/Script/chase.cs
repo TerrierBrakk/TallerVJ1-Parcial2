baa239b0df1a098acb88fec5487366f07c772479
TerrierBrakk <32682025+TerrierBrakk@users.noreply.github.com>
2018-05-25 03:06:11 -0500

    Enemigo animado
    Condicionales de enemigo animado

--- TVFPS/Assets/Script/chase.cs
+++ TVFPS/Assets/Script/chase.cs
@@ -5,24 +5,41 @@ using UnityEngine;
 public class chase : MonoBehaviour {
 
 	public Transform player;
+	static Animator anim;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
 
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		if (Vector3.Distance (player.position, this.transform.position) < 20)
-		{
+		if (Vector3.Distance (player.position, this.transform.position) < 10) {
 			Vector3 direction = player.position - this.transform.position;
 			direction.y = 0;
 			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
 
-			if (direction.magnitude > 3) {
-				this.transform.Translate (0, 0, 0.05f);
+
+			anim.SetBool ("isIddle", false);
+			if (direction.magnitude > 1) {
+				
+				this.transform.Translate (0, 0, 0.1f);
+				anim.SetBool ("isWalking", true);
+				anim.SetBool ("isAttacking", false);
+			} else {
+				anim.SetBool ("isAttacking", true);
+				anim.SetBool ("isWalking", false);
+			}
+		}
+		else
+		{
+				anim.SetBool("isIddle",true);
+				anim.SetBool("isWalking",false);
+				anim.SetBool("isAttacking",false);
+			
 			}
 		}
 	}
-}
